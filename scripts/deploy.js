a707d86a22e756ddff2e67bec4b63e26547aca17
cmevawala-ad <chitrang.mevawala@appdirect.com>
2021-10-12 17:57:58 +0530

    Updated the deploy scripts for Project Space Pool

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,13 +2,45 @@ const { formatUnits, parseUnits } = require('ethers/lib/utils');
 const hre = require('hardhat');
 
 async function main() {
-  // We get the contract to deploy
+  // // Project ICO: We get the contract to deploy
+  // const SpaceCoin = await hre.ethers.getContractFactory('SpaceCoin');
+  // const spacecoin = await SpaceCoin.deploy(parseUnits("500000"));
+  // await spacecoin.deployed();
+  // console.log('SpaceCoin deployed to:', spacecoin.address);
+  
+
+  // Project SpacePool
+  const Treasury = await hre.ethers.getContractFactory('Treasury');
+  const treasury = await Treasury.deploy();
+
+  await treasury.deployed();
+  console.log('Treasury deployed to:', treasury.address);
+
+  const SpaceCoinICO = await hre.ethers.getContractFactory('SpaceCoinICO');
+  const spaceCoinICO = await SpaceCoinICO.deploy(treasury.address);
+
+  await spaceCoinICO.deployed();
+  console.log('SpaceCoinICO deployed to:', spaceCoinICO.address);
+
   const SpaceCoin = await hre.ethers.getContractFactory('SpaceCoin');
-  const spacecoin = await SpaceCoin.deploy(parseUnits("500000"));
+  const spaceCoin = await SpaceCoin.deploy(spaceCoinICO.address, treasury.address);
 
-  await spacecoin.deployed();
+  await spaceCoin.deployed();
+  console.log('SpaceCoin deployed to:', spaceCoin.address);
 
-  console.log('SpaceCoin deployed to:', spacecoin.address);
+  const SpacePoolCoin = await hre.ethers.getContractFactory('SpacePoolCoin');
+  const spacePoolCoin = await SpacePoolCoin.deploy();
+
+  await spacePoolCoin.deployed();
+  console.log('SpacePoolCoin deployed to:', spacePoolCoin.address);
+
+  let overrides = { gasLimit: 5000000 }
+  const SpacePool = await hre.ethers.getContractFactory('SpacePool');
+  const spacePool = await SpacePool.deploy(spaceCoin.address, spacePoolCoin.address, overrides);
+  // const spacePool = await SpacePool.deploy("0xda780737978A6CE02D0807620DA069A59BdFFB6e", "0x010258073B95720014A606a81Dee8B26efabEF8D", overrides);
+
+  await spacePool.deployed();
+  console.log('SpacePool deployed to:', spacePool.address);
 }
 
 main()
